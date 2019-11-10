73f1128a4bde5fc3c3a35a3a73d0aaff03d3c164
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2019-11-09 23:04:48 -0300

    estrutura inicial

---
+++ src/styles/global.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box
+    }
+
+    body {
+        background: #d6656f;
+        text-rendering: optimizeLegibility !important;
+        -webkit-font-smoothing: antialiased !important;
+        -moz-osx-font-smoothing: grayscale !important;
+    }
+`;
+
+export default GlobalStyle;
