cd02a9be4ff67e2f65a7b3319d522454c5c76c14
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2019-11-11 01:38:26 -0300

    finalizando aplicação

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,23 +1,64 @@
 
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
+import moment from 'moment';
 
 import logo from '../../assets/logo.png';
 
 import { Container, Form } from './styles';
 
-import  CompareList from '../../components/compareList'
+import  CompareList from '../../components/compareList';
 
-const Main = () => (
-  <Container>
-    <img src={logo} alt="github compare" />
+export default class Main extends Component {
+  state = {
+    loading: false,
+    repositoryError: false,
+    repositoryInput: '',
+    repositories: [
 
-    <Form>
-      <input type="text" placeholder="repositório" />
-      <button>ok</button>
-    </Form>
+    ]
+  };
 
-    <CompareList />
-  </Container>
-)
+  handleAddRepository = async (e) => {
+    e.preventDefault();
 
-export default Main;
+    this.setState({loading: true})
+
+    try {
+      const { data: repository } = await api.get(`/repos/${this.state.repositoryInput}`);
+
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
+
+      this.setState({
+        repositoryInput: '',
+        repositories: [...this.state.repositories, repository],
+        repositoryError: false
+      })
+    } catch (error) {
+      this.setState({repositoryError: true})
+      console.log(error)
+    } finally {
+      this.setState({loading: false})
+    }
+  }
+
+  render() {
+    return (
+      <Container>
+        <img src={logo} alt="github compare" />
+
+        <Form withError={this.state.repositoryError} onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuário/repositório"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({repositoryInput: e.target.value})}
+          />
+          <button>{ this.state.loading ? <i className="fa fa-spinner fa-pulse"></i>: 'ok' }</button>
+        </Form>
+
+        <CompareList repositories={this.state.repositories} />
+      </Container>
+    )
+  }
+}
